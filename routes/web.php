4db173e5e6d06cf232a17dd190f20c22459b1f83
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-11-12 12:43:10 +0700

    Add authentication layout and login page

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('pages.auth.login');
+})->name('login');
+
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', function () {
         return view('pages.admin.dashboard');
     })->name('dashboard');
 });
+
+
